81ec60d1dfad9ddb1277a76c69c900887873ac74
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-04-21 16:47:45 -0700

    Updated artist table field through db migration

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -60,9 +60,14 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
+    past_shows_count = db.Column(db.Integer, default=0)
+    upcoming_shows_count = db.Column(db.Integer, default=0)
+    
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
