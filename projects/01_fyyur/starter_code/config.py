53f9ff692291db2ec7f4312635e3bc31f4b2bc42
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-04-28 13:36:15 -0700

    Noting that local db has been connected

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+# TODO IMPLEMENT DATABASE URL (Done)
 SQLALCHEMY_DATABASE_URI = 'postgresql://dustinwurtz@localhost:5432/fyyur'
